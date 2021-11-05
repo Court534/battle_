be1583dbc6e38415ac9497b195e2b93bc0dfa7f8
Courtney Stow <courtney@Courtneys-MacBook-Air.local>
2021-11-05 14:28:48 +0000

    Extracting Logic to the Model completed

--- app.rb
+++ app.rb
@@ -25,14 +25,14 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @player_1_name = $player_1
-    @player_2_name = $player_2
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = $player_1
-    @player_2_name = $player_2
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :attack
   end
 
--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,7 @@
 class Player 
   attr_reader :name
 
-  def Initalize(name)
+  def initialize(name)
     @name = name
   end
 end
